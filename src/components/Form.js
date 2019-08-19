4dc0151b5fc19369e3fbe8026bb4c850bc6d6049
Geidy <geidydm@gmail.com>
2019-08-19 14:30:55 -0400

    Modified and fixed the function to clear form, authentificate it and pass state value

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,23 +4,33 @@ import '../css/FormStyle.css';
 class Form extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            value: "",
-                }
         
-
+        this.state = { };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
+
     handleChange(event) {
-        this.setState({value: event.target.value});
-      }
-    
-      handleSubmit(event) {
-        alert('Thank you for your request : ' + this.state.value);
+        this.setState({ [event.target.id]: event.target.value});
+        console.log(event.target)    //always console.log so you can see the changes
+    }
+
+    clearForm() {
+        console.log(this.state)
+        this.setState({ name: '', email: '', college: '', info: '' })
+
+    }
+
+    handleSubmit(event) {
+        alert('Thank you for your request : ' + this.state.name + '; We will send you a response asap at your prefered email address: ' 
+        + this.state.email + '; And  we will request more info from your current college: ' 
+        + this.state.college );
         event.preventDefault();
-      }
+        this.clearForm();
+    }
+
+   
 
 
     render() {
@@ -29,30 +39,30 @@ class Form extends React.Component {
             <div className="Modal">
 
                 <form className="form" onSubmit={this.handleSubmit}>
-                    <div className="modal"> 
-                    <label className="labels">Full Name </label> 
-                    <input type="input" id="name" className="input" value={this.state.name} onChange={this.handleChange} placeholder="Emily Smith" /> 
-                    
+                    <div className="modal">
+                        <label className="labels">Full Name </label>
+                        <input type="input" id="name" className="input" value={this.state.name} onChange={this.handleChange} placeholder="Emily Smith" />
+
                     </div>
-                    <div className="container"> 
-                    <label className="labels">Prefered Email </label>              
-                    <input className="input" id="email" value={this.state.email} onChange={this.handleChange} placeholder="Emily@gmail.com"/>
-                   
+                    <div className="container">
+                        <label className="labels">Prefered Email </label>
+                        <input className="input" id="email" value={this.state.email} onChange={this.handleChange} placeholder="Emily@gmail.com" />
+
                     </div>
-                    <div className="container"> 
-                    <label className="labels">College </label>              
-                    <input className="input" id="email" value={this.state.college} onChange={this.handleChange} placeholder="Lehman College"/>
-                   
+                    <div className="container">
+                        <label className="labels">College </label>
+                        <input className="input" id="college" value={this.state.college} onChange={this.handleChange} placeholder="Lehman College" />
+
                     </div>
                     <div>
-                    <label className="labels">
-                        Info Request:                    
+                        <label className="labels">
+                            Info Request:
                     </label>
-                    <textarea className="textarea" id="info" value={this.state.info} onChange={this.handleChange} placeholder="Info: Study Abroad in Greece" />
-                    
+                        <textarea className="textarea" id="info" value={this.state.info} onChange={this.handleChange} placeholder="Info: Study Abroad in Greece" />
+
                     </div>
-                    
-                    
+
+
                     <button className="btn" type="submit" value="Submit">Submit</button>
                 </form>
 
